a873dce1025bf1020b1fa17e3db0352b243d2649
Gaurav Ochani <GOchani@slb.com>
2020-09-19 12:17:21 +0530

    class 2 notes

---
+++ class12/test.js
@@ -0,0 +1,54 @@
+// uncomment to execute the code-block.
+
+
+
+
+// 5623
+// function reverse(n) {
+//     var digit, new_no = 0
+//     while (n) {
+//         digit = n % 10
+//         new_no = (new_no * 10) + digit
+//         n = parseInt(n / 10)
+//     }
+//     return new_no
+// }
+
+// console.log(reverse(5623))
+
+// console.log(parseInt("56325") + typeof "56325")
+// console.log(56325.2323 + typeof 56325.2323)
+
+// // var i = 0
+// // while (i < 3) {
+// //     console.log("anil")
+// //     i++
+// // }
+
+// // for (let i = 0; i < 3; i++) {
+// //     console.log("anil")
+
+// // }
+
+// // let i = 0
+// // do {
+// //     console.log("anil")
+// //     i++
+// // } while (i < 3);
+
+// //ES5
+// // var anil = function () {
+// //     console.log("anil")
+// // }
+// //ES6
+// // (() => {
+// //     console.log("anil")
+// // })()
+
+
+// let a = 5
+// let b = 10
+// a = a + b
+// b = a - b
+// a = a - b
+// console.log(a, b)

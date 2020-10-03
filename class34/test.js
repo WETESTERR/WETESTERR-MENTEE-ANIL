ecca3b6374e993e4c9f4a5afa421df915f54cba4
Gaurav Ochani <GOchani@slb.com>
2020-10-03 12:24:16 +0530

    class3 notes

---
+++ class34/test.js
@@ -0,0 +1,64 @@
+// uncomment to execute the code-block.
+
+//split
+// var str = "anil"
+// var arr = str.split("")
+// console.log(typeof arr)
+// console.log(typeof arr.join(""))
+
+
+//Array multidimension
+
+// var a = ["anil"]
+// console.log(a.length)
+
+
+
+// var a = [5, 1, [8, "a", 6], 78]
+// console.log(a[2][1])
+
+//Array functions
+// var arr = [5, 1, 8, 7]
+// arr.push(10)
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+// console.log(arr.shift())
+// console.log(arr)
+// arr.unshift(45)
+// console.log(arr)
+
+
+
+// var arr = []
+// var arr = new Array
+
+
+
+
+
+
+
+
+
+
+
+
+//short circuit
+// false || console.log("hello")
+
+
+// var a = 10
+// console.log(!a)
+
+// var a = 10
+// var b = undefined
+// console.log(typeof a, typeof b)
+// console.log(b && a)
+// console.log(a && b)
+
+// var a = 10
+// var b = ""
+// console.log(typeof a, typeof b)
+// console.log(b || a || null)
+// console.log(a || b || 50)
